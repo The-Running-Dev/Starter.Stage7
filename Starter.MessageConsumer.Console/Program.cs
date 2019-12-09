bd665ee21b579319cb0bc9ac48e0ccc867e8ef11
Ben Richards <therunningdev@gmail.com>
2019-12-09 13:45:28 +0100

    Updated Docker builds
    - Base image now uses Alpine for smaller image size
    - Added missing projects were missing to COPY before compile

--- Starter.MessageConsumer.Console/Program.cs
+++ Starter.MessageConsumer.Console/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace Starter.MessageConsumer.Console
+﻿using System.Threading.Tasks;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+
+using Starter.Bootstrapper;
+using Starter.Framework.Entities;
+
+namespace Starter.MessageConsumer.Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var services = new ServiceCollection();
+            Setup.Bootstrap(services);
+
+            var startup = new Startup();
+            var settings = IocWrapper.Instance.GetService<ISettings>();
+
+            var builder = new HostBuilder()
+                .ConfigureLogging(logging =>
+                {
+                    logging.AddConfiguration(startup.Configuration.GetSection("Logging"));
+                    logging.AddConsole();
+                    logging.AddDebug();
+                    logging.AddApplicationInsights(settings.ApplicationInsightsInstrumentationKey);
+                })
+                .ConfigureServices(s =>
+                {
+                    Setup.Bootstrap(s);
+                });
+
+            await builder.RunConsoleAsync();
         }
     }
 }
