105f187987670fd2c8a6b8ad905b3c35db65154a
Boyan Kostadinov <boyan.kostadinov@dervico.co.uk>
2019-12-13 17:28:21 +0100

    Kubernetes Manifest and Ability to Run Docker Local or Azure
    - added kubernetes-cluster support
    - added docker compse environment files for running locally or from azure
    - modified Powershell run script to read the docker compose env files

--- Starter.Build/run.ps1
+++ Starter.Build/run.ps1
@@ -1,14 +1,41 @@
 param(
-	[switch] $AsDaemon = $false
+	[switch] $AsDaemon = $false,
+	[switch] $FromAzure = $false
 )
 
 $command = 'docker-compose'
+$environment = @{$true = 'azure'; $false = 'local';}[$fromAzure -eq $true]
+$environmentVariablesFile = "docker-compose.env.$environment"
 
 $fileSwitch = '-f'
-$composeFile = (Join-Path $PSScriptRoot 'docker-compose.yaml' -Resolve)
+$composeFile = (Join-Path $PSScriptRoot 'docker-compose.yml' -Resolve)
 $upArgument = 'up'
-$detachArgument = @{$true = '-d'; $false = '';}[$AsDaemon -eq $true]
-
+$detachArgument = @{$true = '-d'; $false = '';}[$asDaemon -eq $true]
 $arguments = @($fileSwitch, $composeFile, $upArgument, $detachArgument)
+$dockerImages = @()
+
+# Read the contents of the appropriate environment file, ignoring empty lines
+Get-Content (Join-Path $PSScriptRoot $environmentVariablesFile) | `
+	Where-Object { $_.Trim() -ne '' } | ForEach-Object {
+	# Get the variable and its value by splitting on the plus
+	$variable = $_ -Split '=' | Select-Object -First 1
+	$value = $_ -Split '=' | Select-Object -Last 1
+
+	# Create the variable as an environment variable
+	Set-Item "env:$variable" $value
+
+	# Keep track of the images defined in the environment file
+	if ($variable -match '_image') {
+		$dockerImages += $value
+	}
+}
+
+# For non local environments
+if ($environment -ne 'local') {
+	# Make sure we have the latest docker images
+	$dockerImages | ForEach-Object {
+		docker pull $_
+	}
+}
 
 & $command $arguments
